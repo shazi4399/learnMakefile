c067c18d9a1eb82cda2282316e3e7d6f271cff08
zhanghao <2573344091@qq.com>
2022-09-28 15:43:10 +0800

    尝试test libote未果

---
+++ test_libote/main.cpp
@@ -0,0 +1,11 @@
+//
+// Created by zhanghao on 22-9-27.
+//
+#include <iostream>
+#include "OT_Tests.h"
+using namespace std;
+using namespace tests_libOTe;
+int main(){
+    tests_libOTe::DotExt_Iknp_Test();
+    return 0;
+}
